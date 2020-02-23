991007224b5201df46e8280e347b2ade57cd0239
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2020-02-23 18:42:20 +0100

    add calendar

---
+++ calendar/precache-manifest.849983647bc8268c0c41bb6be0b4b00d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460c094dfc36bec581ad20386633a923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b96733bcc5bd3ad6187",
+    "url": "/static/css/2.3b735098.chunk.css"
+  },
+  {
+    "revision": "467a5d386f1fe8b8227c",
+    "url": "/static/css/main.a04dd7fd.chunk.css"
+  },
+  {
+    "revision": "8b96733bcc5bd3ad6187",
+    "url": "/static/js/2.6dda7f64.chunk.js"
+  },
+  {
+    "revision": "b1acb8ce0d37dd8bdd7138cb02e77a13",
+    "url": "/static/js/2.6dda7f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467a5d386f1fe8b8227c",
+    "url": "/static/js/main.f472b7ae.chunk.js"
+  },
+  {
+    "revision": "2904592037f001e42333",
+    "url": "/static/js/runtime-main.5f9ac8d0.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
